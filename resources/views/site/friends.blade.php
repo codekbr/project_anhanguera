4bace34059ce169dc57f02588e379e9c958db9df
tomoki10vini <guardiandx@yahoo.com>
2021-05-06 23:24:41 -0300

    fix

--- resources/views/site/friends.blade.php
+++ resources/views/site/friends.blade.php
@@ -4,6 +4,10 @@
     <div class="ui container">
         <div class="ui segment form">
             <div class="ui header"> <i class="icon user"></i> Página Amigos</div>
+            <div class="ui icon input">
+                <input type="text" placeholder="Search...">
+                <i class="inverted circular search link icon"></i>
+            </div>
             <div class="ui divider"></div>
             <div class="ui top attached tabular menu">
                 <a class="active item" data-tab="first">Usuários</a>
@@ -11,16 +15,61 @@
             </div>
             <div class="ui bottom attached active tab segment" data-tab="first">
                 <br>
-                <img class="ui middle aligned circular tiny image" src="https://img.ibxk.com.br/2020/03/16/16212433807451.jpeg?w=328">
-                <span>Marcos Vinicius</span> 
-            </div>
+                <div class="ui segment">
+                    <div class="ui two column very relaxed grid">
+                      <div class="column">
+                        <div class="ui segment secondary form">
+                            <div class="ui fields">
+                                <div class="sixteen wide field">
+                                    <div>
+                                        <img class="ui middle aligned circular tiny image" src="{{asset('img/juci6.jpg')}}" >
+                                        <div class="ui compact menu">
+                                            <a class="item">
+                                              <i class="icon user"></i> Amigos
+                                              <div class="floating ui red label">22</div>
+                                            </a>
+                                            <a class="item">
+                                              <i class="icon users"></i> Grupos
+                                              <div class="floating ui teal label">2</div>
+                                            </a>
+                                            <a class="item">
+                                                <i class="user plus icon"></i> Adicionar
+                                            </a>
+                                          </div>
+                                    </div>
+                                </div> 
+                            </div>
+                        </div>
+                      </div>
+                      <div class="column">
+                        <div class="ui segment secondary form">
+                            <div class="ui fields">
+                                <div class="sixteen wide field">
+                                    <img class="ui middle aligned circular tiny image" src="{{asset('img/juci1.jpg')}}" >
+                                    <div class="ui compact menu">
+                                        <a class="item">
+                                          <i class="icon user"></i> Amigos
+                                          <div class="floating ui red label">22</div>
+                                        </a>
+                                        <a class="item">
+                                          <i class="icon users"></i> Grupos
+                                          <div class="floating ui teal label">2</div>
+                                        </a>
+                                        <a class="item">
+                                          <i class="user plus icon"></i> Adicionar
+                                        </a>
+                                      </div>
+                                </div> 
+                            </div>
+                        </div>
+                    </div>
+                      </div>
+                    </div>
+                  </div>
             <div class="ui bottom attached tab segment" data-tab="second">
-                Amigos
+                Amigos 
             </div>
-            
         </div>
-        
-      
     </div>
 @endsection
 @push('scripts')
